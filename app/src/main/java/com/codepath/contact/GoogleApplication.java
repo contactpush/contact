23a5b0061af0ec2e3fb5cb38bd616e7ad68e6009
Emily Zhao <emily@dropbox.com>
2015-03-08 16:24:43 -0700

    Subscribe us to Parse push notifications
    Adds a placeholder icon for push notifications which we can easily
    change later. Subscribes us to broadcast push notifications.

--- app/src/main/java/com/codepath/contact/GoogleApplication.java
+++ app/src/main/java/com/codepath/contact/GoogleApplication.java
@@ -11,7 +11,9 @@ import com.parse.ParseACL;
 import com.parse.ParseCrashReporting;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParsePush;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.parse.SignUpCallback;
 
 public class GoogleApplication extends com.activeandroid.app.Application {
@@ -51,6 +53,21 @@ public class GoogleApplication extends com.activeandroid.app.Application {
         // Optionally enable public read access.
         defaultACL.setPublicReadAccess(true);
         ParseACL.setDefaultACL(defaultACL, true);
+
+        Log.d(TAG, "creating user...");
+        signIntoParse();
+
+        // register device for push notifications by subscribing to a particular channel
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
     }
 
     public static void signIntoParse(String userName, String password, final ParseLoginListener listener){
