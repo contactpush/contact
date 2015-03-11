ba807d70aac2472b055e4d1f63b029c8e3c6be5f
rtteal <rtteal@gmail.com>
2015-03-10 20:50:36 -0700

    Initial version of ProfileFragment persistence to Parse #4.

--- app/src/main/java/com/codepath/contact/fragments/CreateProfileFragment.java
+++ app/src/main/java/com/codepath/contact/fragments/CreateProfileFragment.java
@@ -2,14 +2,43 @@ package com.codepath.contact.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.codepath.contact.R;
+import com.codepath.contact.models.ContactInfo;
+import com.parse.ParseException;
+import com.parse.SaveCallback;
 
 public class CreateProfileFragment extends Fragment {
+    private static final String TAG = "CreateProfileFragment";
     //private OnFragmentInteractionListener mListener;
+    private Button btDone;
+    private ImageView ivProfileImage;
+    private EditText etFirstName;
+    private EditText etMiddleName;
+    private EditText etLastName;
+    private EditText etCompany;
+
+    private Spinner spPhoneType;
+    private EditText etPhone;
+
+    private Spinner spEmailType;
+    private EditText etEmail;
+
+    private Spinner spAddressType;
+    private EditText etAddress;
+
+    private Spinner spSocialProfileType;
+    private EditText etSocialProfile;
+
 
     public static CreateProfileFragment newInstance() {
         CreateProfileFragment fragment = new CreateProfileFragment();
@@ -25,7 +54,64 @@ public class CreateProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_create_profile, container, false);
+        setUpViews(v);
+        btDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save();
+            }
+        });
         return v;
+    }
+
+    private void setUpViews(View v){
+        btDone = (Button) v.findViewById(R.id.btDone);
+        ivProfileImage = (ImageView) v.findViewById(R.id.ivProfileImage);
+        etFirstName = (EditText) v.findViewById(R.id.etFirstName);
+        etMiddleName = (EditText) v.findViewById(R.id.etMiddleName);
+        etLastName = (EditText) v.findViewById(R.id.etLastName);
+        etCompany = (EditText) v.findViewById(R.id.etCompany);
+
+        spPhoneType = (Spinner) v.findViewById(R.id.spPhoneType);
+        etPhone = (EditText) v.findViewById(R.id.etPhone);
+
+        spEmailType = (Spinner) v.findViewById(R.id.spEmailType);
+        etEmail = (EditText) v.findViewById(R.id.etEmail);
+
+        spAddressType = (Spinner) v.findViewById(R.id.spAddressType);
+        etAddress = (EditText) v.findViewById(R.id.etAddress);
+
+        spSocialProfileType = (Spinner) v.findViewById(R.id.spSocialProfileType);
+        etSocialProfile = (EditText) v.findViewById(R.id.etSocialProfile);
+    }
+
+    private void save(){
+        ContactInfo c = new ContactInfo();
+        c.setFirstName(etFirstName.getText().toString());
+        c.setMiddleName(etMiddleName.getText().toString());
+        c.setLastName(etLastName.getText().toString());
+        c.setCompany(etCompany.getText().toString());
+
+        // TODO figure out how to save spinner's position
+        //c.setPhoneType(spPhoneType.get);
+        c.setPhone(etPhone.getText().toString());
+        c.setEmail(etEmail.getText().toString());
+        c.setAddress(etAddress.getText().toString());
+        c.setSocialProfile(etSocialProfile.getText().toString());
+        c.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null){
+                    Toast.makeText(getActivity(), "Save successful", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Save successful!");
+                    getActivity().finish();
+                } else {
+                    Log.e(TAG, "Save failed! " + e.getMessage());
+                    Toast.makeText(getActivity(), "Save failed", Toast.LENGTH_SHORT).show();
+                    getActivity().finish();
+                }
+            }
+        });
     }
 
    /* @Override
--- app/src/main/java/com/codepath/contact/models/ContactInfo.java
+++ app/src/main/java/com/codepath/contact/models/ContactInfo.java
@@ -13,16 +13,61 @@ public class ContactInfo extends ParseObject {
         return getString("userId");
     }
 
+    // TODO refactor to concat first, middle, last
     public String getName() {
         return getString("name");
+    }
+
+    public String getFirstName() {
+        return getString("firstName");
+    }
+
+    public String getMiddleName() {
+        return getString("middleName");
+    }
+
+    public String getLastName() {
+        return getString("lastName");
+    }
+
+    public String getAddressType() {
+        return getString("addressType");
+    }
+
+    public String getAddress() {
+        return getString("address");
+    }
+
+    public String getSocialProfileType() {
+        return getString("socialProfileType");
+    }
+
+    public String getSocialProfile() {
+        return getString("socialProfile");
+    }
+
+    public String getEmailType() {
+        return getString("emailType");
     }
 
     public String getEmail() {
         return getString("email");
     }
 
+    public String getPhoneType() {
+        return getString("phoneType");
+    }
+
     public String getPhone() {
         return getString("phone");
+    }
+
+    public String getProfileImage(){
+        return getString("profileImage");
+    }
+
+    public String getCompany(){
+        return getString("company");
     }
 
     public void setUserId(String userId) {
@@ -33,11 +78,55 @@ public class ContactInfo extends ParseObject {
         put("name", name);
     }
 
+    public void setEmailType(String emailType) {
+        put("emailType", emailType);
+    }
+
     public void setEmail(String email) {
         put("email", email);
     }
 
     public void setPhone(String phone) {
         put("phone", phone);
+    }
+
+    public void setPhoneType(String phoneType) {
+        put("phoneType", phoneType);
+    }
+
+    public void setFirstName(String firstName) {
+        put("firstName", firstName);
+    }
+
+    public void setMiddleName(String middleName) {
+        put("middleName", middleName);
+    }
+
+    public void setLastName(String lastName) {
+        put("lastName", lastName);
+    }
+
+    public void setAddress(String address) {
+        put("address", address);
+    }
+
+    public void setAddressType(String addressType) {
+        put("addressType", addressType);
+    }
+
+    public void setSocialProfile(String socialProfile) {
+        put("socialProfile", socialProfile);
+    }
+
+    public void setSocialProfileType(String socialProfileType) {
+        put("socialProfileType", socialProfileType);
+    }
+
+    public void setProfileImage(String profileImage) {
+        put("profileImage", profileImage);
+    }
+
+    public void setCompany(String company){
+        put("company", company);
     }
 }
