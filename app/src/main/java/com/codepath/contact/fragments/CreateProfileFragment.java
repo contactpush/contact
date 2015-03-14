58b7d165ac4c135a5fecfff88d5b2336f6c63b60
rtteal <rtteal@gmail.com>
2015-03-14 16:39:12 -0700

    initial version of Profile: select photo #18.  User can select a photo. Now need to figure out how to store in Parse.

--- app/src/main/java/com/codepath/contact/fragments/CreateProfileFragment.java
+++ app/src/main/java/com/codepath/contact/fragments/CreateProfileFragment.java
@@ -1,6 +1,14 @@
 package com.codepath.contact.fragments;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,9 +28,16 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateProfileFragment extends Fragment {
     private static final String TAG = "CreateProfileFragment";
+
+    private static final int SELECT_PICTURE_REQUEST_CODE = 3237;
     //private OnFragmentInteractionListener mListener;
     private Button btDone;
     private ImageView ivProfileImage;
@@ -44,6 +59,8 @@ public class CreateProfileFragment extends Fragment {
     private EditText etSocialProfile;
 
     private ContactInfo currentUser;
+
+    private Uri outputFileUri;
 
 
     public static CreateProfileFragment newInstance() {
@@ -158,7 +175,12 @@ public class CreateProfileFragment extends Fragment {
     }
 
     private void setCurrentValues(){
-        // TODO set ivProfileImage...
+        ivProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImageIntent();
+            }
+        });
         etFirstName.setText(currentUser.getFirstName());
         etMiddleName.setText(currentUser.getMiddleName());
         etLastName.setText(currentUser.getLastName());
@@ -187,6 +209,73 @@ public class CreateProfileFragment extends Fragment {
             spSocialProfileType.setSelection(((ArrayAdapter)spSocialProfileType.getAdapter())
                     .getPosition(currentUser.getSocialProfile()));
             etSocialProfile.setText(currentUser.getSocialProfile());
+        }
+    }
+
+    private void openImageIntent() {
+
+        // Determine Uri of camera image to save.
+        final File root = new File(Environment.getExternalStorageDirectory() + File.separator + "MyDir" + File.separator);
+        root.mkdirs();
+        final String fname = "img_" + System.currentTimeMillis() + ".jpg";
+        final File sdImageMainDirectory = new File(root, fname);
+        outputFileUri = Uri.fromFile(sdImageMainDirectory);
+
+        // Camera.
+        final List<Intent> cameraIntents = new ArrayList<>();
+        final Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        final PackageManager packageManager = getActivity().getPackageManager();
+        final List<ResolveInfo> listCam = packageManager.queryIntentActivities(captureIntent, 0);
+        for(ResolveInfo res : listCam) {
+            final String packageName = res.activityInfo.packageName;
+            final Intent intent = new Intent(captureIntent);
+            intent.setComponent(new ComponentName(res.activityInfo.packageName, res.activityInfo.name));
+            intent.setPackage(packageName);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+            cameraIntents.add(intent);
+        }
+
+        // Filesystem.
+        final Intent galleryIntent = new Intent();
+        galleryIntent.setType("image/*");
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+        // Chooser of filesystem options.
+        final Intent chooserIntent = Intent.createChooser(galleryIntent, "Select Source");
+
+        // Add the camera options.
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, cameraIntents.toArray(new Parcelable[cameraIntents.size()]));
+
+        startActivityForResult(chooserIntent, SELECT_PICTURE_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == getActivity().RESULT_OK) {
+            if (requestCode == SELECT_PICTURE_REQUEST_CODE) {
+                final boolean isCamera;
+                if (data == null) {
+                    isCamera = true;
+                } else {
+                    final String action = data.getAction();
+                    if (action == null) {
+                        isCamera = false;
+                    } else {
+                        isCamera = action.equals(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    }
+                }
+
+                Uri selectedImageUri;
+                if (isCamera) {
+                    selectedImageUri = outputFileUri;
+                } else {
+                    selectedImageUri = data == null ? null : data.getData();
+                }
+                if (selectedImageUri != null){
+                    Picasso.with(getActivity()).load(selectedImageUri).into(ivProfileImage);
+                }
+                Log.d(TAG, "imageUri: " + selectedImageUri);
+            }
         }
     }
 
